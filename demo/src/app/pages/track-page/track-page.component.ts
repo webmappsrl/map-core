1e91b8607376a5a77841b758cafcbad56522f210
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2023-05-25 10:30:58 +0200

    feat(demo): track directive added to demo

--- demo/src/app/pages/track-page/track-page.component.ts
+++ demo/src/app/pages/track-page/track-page.component.ts
@@ -1,6 +1,6 @@
 import {Component, ViewEncapsulation, ChangeDetectionStrategy} from '@angular/core';
 import {MAP} from 'demo/src/mocks/conf';
-import {Observable, of} from 'rxjs';
+import {BehaviorSubject, Observable, of} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
@@ -12,9 +12,37 @@ import {HttpClient} from '@angular/common/http';
 })
 export class TrackPageComponent {
   confMAP$ = of(MAP);
+  selectedColor: string = '#caaf15';
+  showMap$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
   track$: Observable<any>;
+  trackColor: string;
+  wmMapTrackColor$: BehaviorSubject<string> = new BehaviorSubject<string>('#caaf15');
 
+  // Colore predefinito
   constructor(private _http: HttpClient) {
     this.track$ = this._http.get('https://geohub.webmapp.it/api/ec/track/30996');
   }
+
+  addTrackColor() {
+    this.showMap$.next(false);
+    this.wmMapTrackColor$.next(this.trackColor);
+    setTimeout(() => {
+      this.showMap$.next(true);
+    }, 300);
+  }
+
+  selectColor(color: string): void {
+    this.showMap$.next(false);
+    this.selectedColor = color;
+    this.wmMapTrackColor$.next(this.selectedColor);
+    setTimeout(() => {
+      this.showMap$.next(true);
+    }, 300);
+  }
+
+  copyFromClipBoard(color: string): void {
+    navigator.clipboard.writeText(color);
+  }
+
+  //TODO: add input for trackElevationChartElements
 }
