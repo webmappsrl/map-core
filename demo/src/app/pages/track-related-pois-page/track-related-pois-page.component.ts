22186878d1705ea45b6d27e1ce066289dcbd9219
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2023-05-29 13:31:03 +0200

    feat(demo): added display of information about the selected poi on track related pois page

--- demo/src/app/pages/track-related-pois-page/track-related-pois-page.component.ts
+++ demo/src/app/pages/track-related-pois-page/track-related-pois-page.component.ts
@@ -15,6 +15,8 @@ export class TrackRelatedPoisPageComponent {
   confMAP$: Observable<any>;
   showMap$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
   track$: Observable<any>;
+  selectedPoi$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  selectedPoiDescription$: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
   constructor(private _http: HttpClient) {
     this.track$ = this._http.get('https://geohub.webmapp.it/api/ec/track/1544');
@@ -23,5 +25,32 @@ export class TrackRelatedPoisPageComponent {
         return conf.MAP;
       }),
     );
+  }
+
+  selectPoi$ = (poiId: number) => {
+    console.log(`selectPoi called with id: ${poiId}`);
+    this.track$.subscribe((track: any) => {
+      console.log('track data:', track);
+
+      const selectedPoi = track?.properties?.related_pois.find(
+        (poi: any) => poi.properties.id === poiId,
+      );
+
+      if (selectedPoi) {
+        console.log('matching poi found:', selectedPoi);
+        this.selectedPoi$.next(selectedPoi);
+        this.selectedPoiDescription$.next(
+          this._removeHtmlTags(selectedPoi?.properties?.description?.it),
+        );
+      } else {
+        console.log(`No poi found with id: ${poiId}`);
+      }
+    });
+  };
+
+  private _removeHtmlTags(html: string): string {
+    let div = document.createElement('div');
+    div.innerHTML = html;
+    return div.innerText;
   }
 }
