9cf2905239ca71d30175219879940e63b8158772
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2023-07-27 10:16:46 +0200

    fix(selected_track_color_is_not_changing): zindex updated for selected tracks

--- src/readonly/zIndex.ts
+++ src/readonly/zIndex.ts
@@ -1,8 +1,8 @@
 export const POSITION_ZINDEX = 510;
 export const CLUSTER_ZINDEX = 500;
+export const SELECTED_TRACK_ZINDEX = 495;
 export const TRACK_ZINDEX = 490;
 export const FEATURE_COLLECTION_ZINDEX = 480;
 export const POINTER_TRACK_ZINDEX = 480;
 export const FLAG_TRACK_ZINDEX = 470;
-export const SELECTED_TRACK_ZINDEX = 460;
 export const ALERT_POI_RADIUS = 100;
