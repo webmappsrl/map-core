663efa4dad16981ee14eee35c2ed9a3cab5c90ab
peppedeka <peppedeka@gmail.com>
2023-11-23 15:23:02 +0100

    ...

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,4 +2,4 @@ declare module 'ol-ext/interaction/SelectCluster';
 declare module 'ol-ext/layer/AnimatedCluster';
 declare module 'ol-ext/geom/ConvexHull';
 declare module 'ol-ext/style/FlowLine';
-declare module 'ol-ext/overlay/popup';
+declare module 'ol-ext/overlay/Popup';
